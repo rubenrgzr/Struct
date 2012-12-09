4453a9c30a22de66e3ed8ba865713a01e2d84577
Ruben Rodriguez <zaiter.ska@gmail.com>
2012-12-09 09:25:00 -0600

    borrado de cache files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new main\dataBundle\maindataBundle(),
+            new main\userBundle\mainuserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {            
